d9a9832a1a7d06777efc799b4e3823a7b50a0252
steph <stephmonte4@gmail.com>
2022-12-17 00:02:23 -0500

    Added comments to ContainsDuplicate.java

--- src/Array/ContainsDuplicate.java
+++ src/Array/ContainsDuplicate.java
@@ -6,10 +6,10 @@ import java.util.Random;
 public class ContainsDuplicate {
     public static void main(String[] args) {
         Random random = new Random();
-        int[] nums = new int[random.nextInt(10 - 1) + 1];
+        int[] nums = new int[random.nextInt(10 - 1) + 1]; // random array length from size 1 to 10
 
         for(int i = 0; i < nums.length; i++) {
-            nums[i] = random.nextInt(10 - -10) + -10;
+            nums[i] = random.nextInt(10 - -10) + -10; // random integer from -10 to 10
         }
 
         printArray(nums);
@@ -23,14 +23,14 @@ public class ContainsDuplicate {
     }
 
     public static boolean solution1(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>(); // use hash set since it does not allow duplicates
 
         for(int num : nums) {
-            if(set.contains(num)) {
-                return true;
-            }
-            set.add(num);
+            if(set.contains(num))
+                return true; // if the set already contains num, then return true
+
+            set.add(num); // else, add num to the set
         }
-        return false;
+        return false; // no duplicates found; all added elements are unique
     }
 }
